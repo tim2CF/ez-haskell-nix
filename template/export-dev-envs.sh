8630b315f95b22a2e427b39a984c6909c308e79c
Ilja Tkachuk <i.tkachuk@sa-84-52-40-203.saturn.infonet.ee>
2020-02-01 20:38:37 +0200

    use capitalized app name in env variables

--- template/export-dev-envs.sh
+++ template/export-dev-envs.sh
@@ -15,7 +15,7 @@ export DATABASE_URL="postgresql:///postgres?host=$PGHOST"
 # app
 #
 
-export TODO_DEFINE_APP_ENV="dev";
-export TODO_DEFINE_APP_LOG_FORMAT="Bracket"; # Bracket | JSON
-export TODO_DEFINE_APP_LIBPQ_CONN_STR="postgresql://nixbld1@/p88lnd";
-export TODO_DEFINE_APP_ENDPOINT_PORT="4000";
+export TODO_DEFINE_APP_CAP_ENV="dev";
+export TODO_DEFINE_APP_CAP_LOG_FORMAT="Bracket"; # Bracket | JSON
+export TODO_DEFINE_APP_CAP_LIBPQ_CONN_STR="postgresql://nixbld1@/p88lnd";
+export TODO_DEFINE_APP_CAP_ENDPOINT_PORT="4000";
